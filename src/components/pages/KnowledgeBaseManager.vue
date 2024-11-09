726285dc0b5dff236ffc4293e15b239736cf32f2
Devin <buptdevin@gmail.com>
2024-11-09 22:38:23 +0800

    11.9 Devin管理员用户操作知识库功能维护

--- src/components/pages/KnowledgeBaseManager.vue
+++ src/components/pages/KnowledgeBaseManager.vue
@@ -1,4 +1,5 @@
 <template>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
   <div class="app-container">
     <!-- 侧边栏 -->
     <div class="sidebar">
