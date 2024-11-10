c7fb106c5d9be60c02709955be154b8acb935e55
Devin <buptdevin@gmail.com>
2024-11-11 01:15:02 +0800

    11.11 Devin管理员用户操作知识库功能维护

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,12 @@ import router from './router';
 import ElementPlus from 'element-plus';
 import 'element-plus/dist/index.css';
 import '@vue-flow/core/dist/style.css';
+import Antd from 'ant-design-vue';
 import axios from "axios";
 
 axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('access_token')}`;
 const app = createApp(App);
 app.use(router);
 app.use(ElementPlus);
+app.use(Antd);
 app.mount('#app');
